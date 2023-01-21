dadbd929ebf18ef31aab85d176a1b411f8c4ae19
Hae Choi <haechoi92@yahoo.com>
2023-01-20 21:33:59 -0800

    add script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 "use strict";
 
 const shareBtn = document.querySelector(".icon-share");
+const shareBtnMobile = document.querySelector(".icon-share-mobile");
 const bubble = document.querySelector(".bubble");
 const color = document.querySelector(".icon-img");
+const shareFooter = document.querySelector(".share-footer");
 
 shareBtn.addEventListener("click", () => {
-  shareBtn.classList.toggle("changeBg");
-  color.classList.toggle("changeColor");
-  bubble.classList.toggle("fade");
+  // WEEEEEEEEEEE
+  if (window.matchMedia("(max-width: 726px)").matches) {
+    shareFooter.classList.toggle("hidden");
+  } else {
+    shareBtn.classList.toggle("changeBg");
+    color.classList.toggle("changeColor");
+    bubble.classList.toggle("fade");
+  }
+});
+
+shareBtnMobile.addEventListener("click", () => {
+  shareFooter.classList.toggle("hidden");
 });
