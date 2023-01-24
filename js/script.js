6d2dcc77ecf436ff4cb397b831871390c519ce11
Hae Choi <haechoi92@yahoo.com>
2023-01-23 16:59:11 -0800

    add readme

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 "use strict";
-
+// BTNS
 const shareBtn = document.querySelector(".icon-share");
 const shareBtnMobile = document.querySelector(".icon-share-mobile");
+// POPUPS
 const bubble = document.querySelector(".bubble");
-const color = document.querySelector(".icon-img");
 const shareFooter = document.querySelector(".share-footer");
+// BG ICON COLOR
+const color = document.querySelector(".icon-img");
 
 shareBtn.addEventListener("click", () => {
-  // WEEEEEEEEEEE
+  // When user clicks the share button two things can happen; if browser width <= 726px, unhide footer share.
   if (window.matchMedia("(max-width: 726px)").matches) {
     shareFooter.classList.toggle("hidden");
+    // If > 726px, unhide bubble popup and change icon bg color and icon fill.
   } else {
     shareBtn.classList.toggle("changeBg");
     color.classList.toggle("changeColor");
@@ -17,6 +20,8 @@ shareBtn.addEventListener("click", () => {
   }
 });
 
+// This separate btn only appears when sharefooter is toggle on
+// When user clicks, exits footer share
 shareBtnMobile.addEventListener("click", () => {
   shareFooter.classList.toggle("hidden");
 });
